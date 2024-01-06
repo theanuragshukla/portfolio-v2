d7a7d3a25a8aa5ee6613bc657293545298b97c01
theanuragshukla <tachyonanurag@gmail.com>
2024-01-06 14:16:06 +0530

    🔥 minor changes

--- portfolio/src/app/data/client.js
+++ portfolio/src/app/data/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL:process.env.REACT_APP_SERVER_URL,
+  baseURL:process.env.REACT_APP_SERVER_URL || "http://localhost:8000",
   withCredentials:true
 })
 
