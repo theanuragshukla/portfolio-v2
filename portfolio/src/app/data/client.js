84f69705ccdca4fe143030e08d279c44b4ab1e4a
theanuragshukla <tachyonanurag@gmail.com>
2025-04-20 20:23:46 +0000

    fix: server url

--- portfolio/src/app/data/client.js
+++ portfolio/src/app/data/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL:process.env.REACT_APP_SERVER_URL || "http://localhost:8000",
+  baseURL:process.env.REACT_APP_SERVER_URL || "https://portserver.anurags.dev",
   withCredentials:true
 })
 
